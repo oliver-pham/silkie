626edf55b2fe357afc297306aeba23665433b186
Phu Thong Pham <ptpham4@myseneca.ca>
2021-11-17 13:44:19 -0500

    Added test for config file with custom stylesheet

--- tests/integration/test_config.py
+++ tests/integration/test_config.py
@@ -12,7 +12,17 @@ from silkie.silkie import get_html
         (
             str(Path("tests", "data", "config", "single_text_file.json")),
             Path("tests", "fixture", "text", "Lorem Ipsum.html").read_text().strip(),
-        )
+        ),
+        (
+            str(
+                Path(
+                    "tests", "data", "config", "single_text_file_custom_stylesheet.json"
+                )
+            ),
+            Path("tests", "fixture", "text", "Lorem Ipsum (custom stylesheet).html")
+            .read_text()
+            .strip(),
+        ),
     ],
 )
 def test_config_to_html(config_file_path, expected_output_html):
