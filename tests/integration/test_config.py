c99250ae5de74eb227639f973d9949034c119954
Phu Thong Pham <ptpham4@myseneca.ca>
2021-11-17 13:37:12 -0500

    Fixed & Added test for JSON config files

---
+++ tests/integration/test_config.py
@@ -0,0 +1,21 @@
+import pytest
+
+from pathlib import Path
+
+from silkie.options import GeneratorOptions
+from silkie.silkie import get_html
+
+
+@pytest.mark.parametrize(
+    "config_file_path, expected_output_html",
+    [
+        (
+            str(Path("tests", "data", "config", "single_text_file.json")),
+            Path("tests", "fixture", "text", "Lorem Ipsum.html").read_text().strip(),
+        )
+    ],
+)
+def test_config_to_html(config_file_path, expected_output_html):
+    options = GeneratorOptions(input_path=None, config_file_path=config_file_path)
+    output_html = get_html(options).strip()
+    assert output_html == expected_output_html
